34d8e9eedfd8ccfdf141679841012db3b62dec46
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-03-25 23:46:54 -0500

    RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { CountryModule } from './country/country.module';
+import { AppRouteingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule, CountryModule],
+  imports: [BrowserModule, AppRouteingModule, SharedModule, CountryModule],
   providers: [],
   bootstrap: [AppComponent],
 })
