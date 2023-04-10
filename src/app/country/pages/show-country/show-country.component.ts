08b59b013be7336cdbd02816e19915a65e6e675e
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-04-09 22:32:28 -0500

    show country independently

--- src/app/country/pages/show-country/show-country.component.ts
+++ src/app/country/pages/show-country/show-country.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-show-country',
   templateUrl: './show-country.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class ShowCountryComponent {
+export class ShowCountryComponent implements OnInit {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countryService: CountryService
+  ) {}
 
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(({ id }) => {
+      console.log(id);
+
+      this.countryService.getCountryByAlpha(id).subscribe((country) => {
+        console.log(country);
+      });
+    });
+  }
 }
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -20,4 +20,9 @@ export class CountryService {
     const url = `${this.apiUrl}/capital/${capital}`;
     return this.http.get<Country[]>(url);
   }
+
+  getCountryByAlpha(id: string): Observable<Country> {
+    const url = `${this.apiUrl}/alpha/${id}`;
+    return this.http.get<Country>(url);
+  }
 }
