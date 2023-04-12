0d453d775d573296d0e7448105f833333261486a
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-04-11 23:21:45 -0500

    text autocomplete

--- src/app/country/pages/by-country/by-country.component.ts
+++ src/app/country/pages/by-country/by-country.component.ts
@@ -5,16 +5,25 @@ import { Country } from '../../interfaces/country.interface';
 @Component({
   selector: 'app-by-country',
   templateUrl: './by-country.component.html',
-  styles: [],
+  styles: [
+    `
+      .pointer {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
 export class ByCountryComponent {
   search: string = '';
   thereIsError: boolean = false;
   countries: Country[] = [];
+  suggestedCountries: Country[] = [];
+  showSuggestions: boolean = false;
 
   constructor(private countryService: CountryService) {}
 
   onSubmit(search: string): void {
+    this.showSuggestions = false;
     this.thereIsError = false;
     this.countryService.searchCountry(search).subscribe({
       next: (data) => {
@@ -32,6 +41,19 @@ export class ByCountryComponent {
 
   suggestions(search: string) {
     this.thereIsError = false;
-    // TODO: Create a new suggestion
+    this.search = search;
+    if (search === '') {
+      this.showSuggestions = false;
+    } else {
+      this.showSuggestions = true;
+    }
+    this.countryService.searchCountry(search).subscribe({
+      next: (countries) => (this.suggestedCountries = countries.splice(0, 5)),
+      error: (err) => (this.suggestedCountries = []),
+    });
+  }
+
+  searchSuggested(search: string) {
+    this.onSubmit(search);
   }
 }
