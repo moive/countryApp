92a6f82f7dedac9e17aa8e4f5edcc040070bb855
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-04-11 21:26:52 -0500

    ngClas - class - [class] and class conditional

--- src/app/country/pages/by-region/by-region.component.ts
+++ src/app/country/pages/by-region/by-region.component.ts
@@ -3,9 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-by-region',
   templateUrl: './by-region.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ByRegionComponent {
+  regions: string[] = ['americas', 'asia', 'europe', 'africa', 'oceania'];
+  activeRegion: string = '';
 
+  constructor() {}
+
+  getClassCSS(region: string) {
+    return region === this.activeRegion
+      ? 'btn btn-primary'
+      : 'btn btn-outline-primary';
+  }
+
+  activateRegion(region: string) {
+    this.activeRegion = region;
+
+    // TODO: call for services
+  }
 }
